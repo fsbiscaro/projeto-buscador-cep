01bd4d497b2af60fb978854b5f6d4f7f25aff038
fsbiscaro <fsbiscaro@hotmail.com>
2023-03-29 20:39:32 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component }  from 'react';
+import { FiSearch } from 'react-icons/fi';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+     <div className="containerInput">
+        <input type="text"
+        placeholder="Digite seu cep..."/>
+
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+        </button> 
+     </div>
+
+        <main className="main">
+          <h2>CEP: 79003222</h2>
+
+          <span>Rua Teste</span>
+          <span>Complemento: </span>
+          <span>Vila Rosa </span>
+          <span>Estado </span>
+
+        </main>
+
+
+    </div>
+  );
+  
+}
+
+export default App;
